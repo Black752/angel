42753f4e7b79ceb2af7cf7b4aa98fd9a17e8cbe0
Black752 <66740742+Black752@users.noreply.github.com>
2020-06-10 22:30:47 +0500

    Update blackangel.py

--- blackangel.py
+++ blackangel.py
@@ -56,10 +56,10 @@ logo = """ -----------------------------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBasit Rajpoot
+\033[1;96mInstagram \033[1;93m: \033[1;Black Angel
+\033[1;96mFacebook  \033[1;93m: \033[1; White Devil
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/blackangel/zero
 \033[1;91m======================================="""
 
 def tik():
@@ -80,10 +80,10 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mBasit Rajpoot
+\033[1;96mInstagram \033[1;93m: \033[1;92mBlack Angel
+\033[1;96mFacebook  \033[1;93m: \033[1;92m White Devil
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/blackangel/zero
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
